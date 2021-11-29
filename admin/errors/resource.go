d38d47b6250798d15df7f796455a1856c4931228
anhnguyen0809 <anhnnp0809@gmail.com>
2021-11-29 15:26:44 +0700

    change admin -> system

--- admin/errors/resource.go
+++
@@ -1,7 +0,0 @@
-package errors
-
-import "rin-echo/common"
-
-var (
-	ERR_RESOURCE_SLUG_EXISTS = common.NewRinError("resource_slug_exists", "Slug already exists.")
-)
--- admin/errors/user.go
+++
@@ -1,9 +0,0 @@
-package errors
-
-import "rin-echo/common"
-
-var (
-	ERR_USERNAME_EXISTS             = common.NewRinError("username_exists", "Username already exists in our system.")
-	ERR_USERNAME_PASSWORD_NOT_MATCH = common.NewRinError("username_password_not_match", "Your username and password didn't match.")
-	ERR_PASSWORD_REQUIRED           = common.NewRinError("password_required", "Password is a required field")
-)
